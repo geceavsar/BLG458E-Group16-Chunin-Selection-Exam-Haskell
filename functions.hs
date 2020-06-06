e44b98654faa3937425ea9ec5b20dc93f707c1c8
Ayse Sayin <aysesayin7@gmail.com>
2020-06-06 23:11:41 +0300

    writing fight function

--- functions.hs
+++ functions.hs
@@ -8,22 +8,22 @@ import Data.Char
 
 data Ninja=Ninja{name::String,country::Char,score::Float,
 status::String,exam1::Float,
-exam2::Float,ability1::String,
-ability2::String,r::Int} deriving (Eq,Show)
+exam2::Float,ability1::Float,
+ability2::Float,r::Int} deriving (Eq,Show)
 
 ninjas::[Ninja]
 
 naruto::Ninja
-naruto = Ninja{name="Naruto", country='f', status="Junior", exam1=40, exam2=45, ability1="Clone", ability2="Summon", score=0, r=0}
+naruto = Ninja{name="Naruto", country='f', status="Junior", exam1=40, exam2=45, ability1=20, ability2=50, score=0, r=0}
 
 sasuke::Ninja
-sasuke = Ninja{name="Sasuke", country='f', status="Junior", exam1=50, exam2=60, ability1="Lightning", ability2="Fire",score=0, r=0}
+sasuke = Ninja{name="Sasuke", country='f', status="Junior", exam1=50, exam2=60, ability1=50, ability2=40,score=0, r=0}
 
 gaara::Ninja
-gaara=Ninja{name="Gaara", country='n', status="Junior", exam1=55, exam2=80, ability1="Vision", ability2="Sand",score=0, r=0}
+gaara=Ninja{name="Gaara", country='n', status="Junior", exam1=55, exam2=80, ability1=30, ability2=50,score=0, r=0}
 
 temari::Ninja
-temari=Ninja{name="Temari", country='n', status="Junior", exam1=40, exam2=60, ability1="Hit", ability2="Blade",score=0, r=0}
+temari=Ninja{name="Temari", country='n', status="Junior", exam1=40, exam2=60, ability1=10, ability2=20,score=0, r=0}
 
 
 ninjas = [naruto, sasuke, gaara, temari]
@@ -68,22 +68,26 @@ updateScoreHelper winner country = result where
 
 deleteLoser :: Ninja -> [Ninja]
 deleteLoser loser
-	| country loser == 'f' =  filter(\n -> name n /= name loser) fire 
-	| country loser == 'e' =  filter(\n -> name n /= name loser) earth 
-	| country loser == 'w' =  filter(\n -> name n /= name loser) water
-	| country loser == 'l' =  filter(\n -> name n /= name loser) lightning
-	| country loser == 'n' =  filter(\n -> name n /= name loser) wind
+ | country loser == 'f' =  filter(\n -> name n /= name loser) fire 
+ | country loser == 'e' =  filter(\n -> name n /= name loser) earth 
+ | country loser == 'w' =  filter(\n -> name n /= name loser) water
+ | country loser == 'l' =  filter(\n -> name n /= name loser) lightning
+ | country loser == 'n' =  filter(\n -> name n /= name loser) wind
 
 
-round :: Ninja -> Ninja -> (Ninja,Ninja)
-round ninja1 ninja2 (winner,loser)
+fight :: Ninja -> Ninja -> (Ninja,Ninja)
+fight ninja1 ninja2 
 	| score ninja1 > score ninja2 = (ninja1, ninja2)
 	| score ninja1 < score ninja2 = (ninja2, ninja1)
 	| otherwise = (if ability1 ninja1 + ability2 ninja1 > ability1 ninja2 + ability2 ninja2 then (ninja1, ninja2) 
-        else {-if ability1 ninja1 + ability2 ninja1 < ability1 ninja2 + ability2 ninja2 then-} (ninja2, ninja1))
-        {-else (if number == 0 then 
-            (ninja1,ninja2) else (ninja2,ninja1) where number :: IO Int
-                    number = getStdRandom (randomR (0,1)) )-}
+        else if ability1 ninja1 + ability2 ninja1 < ability1 ninja2 + ability2 ninja2 then (ninja2, ninja1)
+        else (if number == 0 then 
+            (ninja1,ninja2) else (ninja2,ninja1)	where 
+				number :: IO Int
+				number = getStdRandom (randomR (0,1)) ))
+
+
+
 
 --manipulateList :: [Ninja] 
 
