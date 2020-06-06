2f041a11d5995ad240396ca7e26fd1890f0b620b
Gizem Ece Avsar <avsar@itu.edu.tr>
2020-06-06 18:05:34 +0300

    uscore update for winner country

--- functions.hs
+++ functions.hs
@@ -18,10 +18,10 @@ sasuke::Ninja
 sasuke = Ninja{name="Sasuke", country='f', status="Junior", exam1=50, exam2=60, ability1="Lightning", ability2="Fire",score=0, r=0}
 
 gaara::Ninja
-gaara=Ninja{name="Gaara", country='w', status="Junior", exam1=55, exam2=80, ability1="Vision", ability2="Sand",score=0, r=0}
+gaara=Ninja{name="Gaara", country='n', status="Junior", exam1=55, exam2=80, ability1="Vision", ability2="Sand",score=0, r=0}
 
 temari::Ninja
-temari=Ninja{name="Temari", country='w', status="Junior", exam1=40, exam2=60, ability1="Hit", ability2="Blade",score=0, r=0}
+temari=Ninja{name="Temari", country='n', status="Junior", exam1=40, exam2=60, ability1="Hit", ability2="Blade",score=0, r=0}
 
 
 ninjas = [naruto, sasuke, gaara, temari]
@@ -31,8 +31,15 @@ fire = [ n | n <- ninjas, country n == 'f']
 
 
 wind::[Ninja]
-wind=[ n | n <- ninjas, country n == 'w']
+wind=[ n | n <- ninjas, country n == 'n']
 
+
+lightning::[Ninja]
+lightning=[]
+water::[Ninja]
+water=[]
+earth::[Ninja]
+earth=[]
 
 checkIfValid :: String -> Char -> Bool
 checkIfValid nm cnt = result where 
@@ -40,6 +47,21 @@ checkIfValid nm cnt = result where
     secondList = filter(\n -> country n == cnt) firstList
     firstList = filter(\n -> name n == nm) ninjas
 
---result = checkIfValid "Naruto" 'f'
+updateScore :: Ninja -> [Ninja]
+updateScore winner
+    | country winner == 'f' = updateScoreHelper winner fire 
+    | country winner == 'e' = updateScoreHelper winner earth 
+    | country winner == 'w' = updateScoreHelper winner water
+    | country winner == 'l' = updateScoreHelper winner lightning 
+    | country winner == 'n' = updateScoreHelper winner wind
+
+
+updateScoreHelper :: Ninja -> [Ninja] -> [Ninja]
+updateScoreHelper winner country = result where
+    result = [newNinja] ++ filter(\n -> name n /= name winner) country
+    newNinja = winner { score = score winner + 10}
+
+--updateScore naruto
+
 
 
