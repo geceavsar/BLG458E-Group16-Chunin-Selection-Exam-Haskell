e6ceb1af93120a314a213eed35c75e30f1af5f7e
pelingumuslu <pelingumuslu8@gmail.com>
2020-06-07 22:12:13 +0300

    Add files via upload

--- cse.hs
+++ cse.hs
@@ -6,10 +6,11 @@ import Data.Char
 import System.Exit (exitSuccess)
 
 --ninja information
-data Ninja=Ninja{name::String,country::Char,score::Float,
+data Ninja=Ninja{name::String,country::String,score::Float,
 status::String,exam1::Float,
 exam2::Float,ability1::String,
 ability2::String,r::Int} deriving (Eq,Show)
+
 --country list
 fire::[Ninja]
 fire=[]
@@ -23,9 +24,12 @@ earth::[Ninja]
 earth=[]
 ninjaStr::[String]
 ninjaStr=[]
+ninjas::[Ninja]
+ninjas=[]
 
 --functions//algebraic data types
 data Function = OneCountryNinjas | AllCountriesNinjas | MakeRoundNinjas |MakeRoundCountries |Exit deriving Show
+
 --choose a function
 pickFunction::String->Function
 pickFunction f=case f of 
@@ -40,12 +44,24 @@ pickFunction f=case f of
 main=do
  contents<-readFile "csereport.txt"
  let listofNinjas = lines contents
-     ninjaStr = zipWith (\n line -> show line) [0..] listofNinjas
+     ninjaStr = zipWith (\n line -> show line) [0..] listofNinjas     
  putStrLn $ unlines ninjaStr
  menu 
  --putStrLn "These are my ninjas:"
  --putStrLn $ unlines ninjaStr
+ 
+convert2Ninja::[String]->[Ninja]
+convert2Ninja ninjaStr
+ |null  ninjaStr= []
+ |otherwise = myparser (head ninjaStr)
+ where
+  myparser::[String]->[Ninja]
+  myparser s = Ninja s !! 0 s !! 1 getScore (toFloat s !! 2) (toFloat s !! 3) s !! 4 s !! 5 "Junior" (read (s !! 2)::Float) (toFloat s !! 3) s !! 4 s !! 5 0.0) ++ ninjas
 
+toFloat::String->Float
+toFloat s = read s::Float
+
+--print main menu
 menu::IO()
 menu=do
  putStr  "a)View a Country's Ninja Information\nb)View All Countries' Ninja Information\nc)Make Round Between Ninjas\nd)Make Round Between Countries\ne)Exit\nEnter the action: \n"
@@ -54,17 +70,11 @@ menu=do
   let c=pickFunction choise
   case c of
    OneCountryNinjas->oneCountryNinjas
-  --AllCountriesNinjas->do
-  --MakeRoundNinjas->do
-  --MakeRoundCountries->do
+  --AllCountriesNinjas->allCounrtiesNinjas
+  --MakeRoundNinjas->makeRoundNinjas
+  --MakeRoundCountries->makeRoundCountries
    Exit->exit
 
-exit::IO()
-exit=do
- putStrLn "Final List:"
- putStrLn $ unlines ninjaStr
- return()
- 
 --in this function user enters a counrty code and required countrys' ninjas printed
 oneCountryNinjas::IO()
 oneCountryNinjas=do
@@ -82,6 +92,12 @@ oneCountryNinjas=do
    "N"->print wind
    "f"->print fire
    "F"->print fire
+--exit function
+exit::IO()
+exit=do
+ putStrLn "Final List:"
+ putStrLn $ unlines ninjaStr
+ return()
    
 --abilities as score equivalents
 abilityToPoint::String->Float
