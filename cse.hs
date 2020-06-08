3b7e8037d7afd413e1771d1c49a1564221387e77
pelingumuslu <pelingumuslu8@gmail.com>
2020-06-09 01:04:09 +0300

    Add files via upload

--- cse.hs
+++ cse.hs
@@ -4,7 +4,7 @@ import System.Directory
 import Data.List  
 import Data.Char
 import System.Exit (exitSuccess)
-
+import Control.Monad
 --ninja information
 data Ninja=Ninja{name::String,country::String,score::Float,
 status::String,exam1::Float,
@@ -12,7 +12,7 @@ exam2::Float,ability1::String,
 ability2::String,r::Int} deriving (Eq,Show)
 
 --country list
-fire::[Ninja]
+{-fire::[Ninja]
 fire=[]
 ligthning::[Ninja]
 ligthning=[]
@@ -22,16 +22,12 @@ wind::[Ninja]
 wind=[]
 earth::[Ninja]
 earth=[]
---ninjaStr::[String]
---ninjaStr=[]
---ninjas::[Ninja]
---ninjas=[]
+-}
 
-
---functions//algebraic data types
+--functions as algebraic data types
 data Function = OneCountryNinjas | AllCountriesNinjas | MakeRoundNinjas |MakeRoundCountries |Exit deriving Show
 
---choose a function
+--when user enters one of the strings below(a,b,c,d,e) related function will be called
 pickFunction::String->Function
 pickFunction f=case f of 
     "a"->OneCountryNinjas
@@ -40,67 +36,44 @@ pickFunction f=case f of
     "d"->MakeRoundCountries
     "e"->Exit
 
---prints the initial list
---this function also splits data line by line and store it in list 
+--our main function
 main=do
- contents<-readFile "csereport.txt"
- let listofNinjas = lines contents
- --getNinjaFromString . words
- let ninjas = parseNinjas listofNinjas
- print (ninjas)
-	
--- print (words (head listofNinjas))
- {-getNinja :: IO String
- getNinja = do line <- listofNinjas
-					return (read line :: String)-}
-
- 
- --let read (words (head listofNinjas)) :: String
+ contents<-readFile "csereport.txt"--reads from file
+ let listofNinjas = lines contents--ignores newline characters and stores lines in a list
+ let ninjas = convertToNinjas listofNinjas--each line that stored is converted to ninja
  menu 
+ do
+  choise<-getLine
+  let c=pickFunction choise
+  case c of
+    OneCountryNinjas->oneCountryNinjas ninjas
+    AllCountriesNinjas->allCountryNinjas ninjas
+    MakeRoundNinjas->makeRoundNinjas ninjas
+   --MakeRoundCountries->makeRoundCountries
+    Exit->exit
 
-{-
-content <- readFile (head args) -- get first arg as the file name, read all file to contents
-            let linesOfNinjas = lines content -- split contents line by line
-            
-            let ninjas = parseNinjas linesOfNinjas
-------burada okuma yapıyor
--}
+makeRoundNinjas::[Ninja]->(Ninja,Ninja)
+makeRoundNinjas=do
+ putStrLn "Enter the name of the first ninja: "
+ name<-getLine
+ putStrLn "Enter the country code of the first ninja: "
+ countryCode<-getLine
+ putStrLn "Enter the name of the second ninja: "
+ name2<-getLine
+ putStrLn "Enter the country code of the second ninja: "
+ countryCode2<-getLine
+ fight [n|n <- ninjas, name n==name] [n|n <- ninjas, name n==name2]
 
-parseNinjas :: [String] -> [Ninja] -- Get Ninjas from given String array
-parseNinjas [] = []
-parseNinjas (x:xs) = (scoreUpdate . getNinjaFromString . words) x : parseNinjas(xs)  
+fight :: Ninja -> Ninja -> (Ninja,Ninja)
+fight ninja1 ninja2 
+    | score ninja1 > score ninja2 = (ninja1, ninja2)
+    | score ninja1 < score ninja2 = (ninja2, ninja1)
+    | otherwise = (if ability1 ninja1 + ability2 ninja1 >= ability1 ninja2 + ability2 ninja2 then (ninja1, ninja2) else (ninja2, ninja1))
 
-
-getNinjaFromString :: [String] -> Ninja
-getNinjaFromString line = Ninja { -- Create a Ninja element with given String array, convert data types if necessary
-name = (line !! 0), -- Name is the first value in csereport
-country = (line !! 1), -- country is the second string, but Ninja data type only requires the first character
-exam1 = toFloat(line !! 2),  -- exam1 is the third string which is then converted to Float
-exam2 = toFloat(line !! 3), -- exam2 is the fourth string which is then converted to Float
-ability1 = (line !! 4), -- ability1 is the fifth string 
-ability2 = (line !! 5), -- ability2 is the sixth string 
-status = "Junior", -- Initial value is junior
-r = 0 , -- Initial round number is zero
-score = 0 -- Set score initially to zero (This is because we couldn't access the exam1 exam2 ability1 ability2 attributes of this object while creating)
-}
-
-  
-
-scoreUpdate :: Ninja -> Ninja
-scoreUpdate ninja = updatedNinja where
-	updatedNinja =  ninja {score = getScore (exam1 ninja) (exam2 ninja) (ability1 ninja) (ability2 ninja)} where 
-	    getScore::Float->Float->String->String->Float
-	    getScore e1 e2 a1 a2 = 0.5*e1 + 0.3*e2 + abilityToPoint a1 + abilityToPoint a2
-
-toFloat::String->Float
-toFloat s = read s::Float
-
-
---print main menu
 menu::IO()
 menu=do
  putStr  "a)View a Country's Ninja Information\nb)View All Countries' Ninja Information\nc)Make Round Between Ninjas\nd)Make Round Between Countries\ne)Exit\nEnter the action: \n"
- do 
+ {-do 
   choise<-getLine
   let c=pickFunction choise
   case c of
@@ -109,32 +82,56 @@ menu=do
   --MakeRoundNinjas->makeRoundNinjas
   --MakeRoundCountries->makeRoundCountries
    Exit->exit
+-}
+convertToNinjas :: [String] -> [Ninja]--string to ninja
+convertToNinjas [] = []--if empty return 
+convertToNinjas (x:xs) = (scoreUpdate . getNinja . words) x : convertToNinjas(xs)--each line is divided into sublist without spaces and obtained string list is sent to getNinja and returns as ninja type 
+
+
+getNinja :: [String] -> Ninja
+getNinja line = Ninja {name = (line !! 0),country = (line !! 1),exam1 = toFloat(line !! 2),exam2 = toFloat(line !! 3),ability1 = (line !! 4),ability2 = (line !! 5),status = "Junior",r = 0,score=0}
+
+  
+
+scoreUpdate :: Ninja -> Ninja
+scoreUpdate ninja = updatedNinja where
+ updatedNinja =  ninja {score = getScore (exam1 ninja) (exam2 ninja) (ability1 ninja) (ability2 ninja)} where 
+   getScore::Float->Float->String->String->Float
+   getScore e1 e2 a1 a2 = 0.5*e1 + 0.3*e2 + abilityToPoint a1 + abilityToPoint a2
+
+toFloat::String->Float
+toFloat s = read s::Float
+
+
 
 --in this function user enters a counrty code and required countrys' ninjas printed
-oneCountryNinjas::IO()
-oneCountryNinjas=do
+oneCountryNinjas::[Ninja]->IO()
+oneCountryNinjas ninjas=do
  putStrLn "Enter the country code: "
  do
   code<-getLine
   case code of
-   "e"->print earth
-   "E"->print earth
-   "l"->print ligthning
-   "L"->print ligthning
-   "w"->print water
-   "W"->print water
-   "n"->print wind
-   "N"->print wind
-   "f"->print fire
-   "F"->print fire
+   "e"->print [ n | n <- ninjas, country n == "Earth"]
+   "E"->print [ n | n <- ninjas, country n == "Earth"]
+   "l"->print [ n | n <- ninjas, country n == "Lightning"]
+   "L"->print [ n | n <- ninjas, country n == "Lightning"]
+   "w"->print [ n | n <- ninjas, country n == "Water"]
+   "W"->print [ n | n <- ninjas, country n == "Water"]
+   "n"->print [ n | n <- ninjas, country n == "Wind"]
+   "N"->print [ n | n <- ninjas, country n == "Wind"]
+   "f"->print [ n | n <- ninjas, country n == "Fire"]
+   "F"->print [ n | n <- ninjas, country n == "Fire"]
+
+allCountryNinjas::[Ninja]->IO()
+allCountryNinjas ninjas=print ninjas
+
 --exit function
 exit::IO()
 exit=do
  putStrLn "Final List:"
- --putStrLn $ unlines ninjaStr
  return()
    
---abilities as score equivalents
+--we take abilities as string and return their float equivalents which are already given in the assignment sheet
 abilityToPoint::String->Float
 abilityToPoint str
  |str=="Clone" = 20
@@ -149,9 +146,6 @@ abilityToPoint str
  |str=="Storm" = 10
  |str=="Rock" = 20
  |otherwise=0
-
---this is a higher order function cause it returns a function as result
-
 
 
 
